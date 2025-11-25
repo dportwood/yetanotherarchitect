ea348963e2562c7e3348df710bb3be718fd14dc0
dportwood <122243184+dportwood@users.noreply.github.com>
2025-11-25 13:31:12 -0600

    Add script to sync on-prem AD user with Entra ID

---
+++ GetImmuatableOPWriteEntra.ps1
@@ -0,0 +1,39 @@
+Import-Module ActiveDirectory
+
+# 1. Get on-premises AD user object-GUID
+$userUPN = "user@domain.com"
+$adUser = Get-ADUser -Identity $userUPN -Properties objectGUID
+
+if (-not $adUser) {
+    Write-Error "On-prem user not found for UPN $userUPN"
+    return
+}
+
+$guidBytes = $adUser.objectGUID.ToByteArray()
+$immutableId = [Convert]::ToBase64String($guidBytes)
+Write-Host "Calculated immutable ID for $userUPN: $immutableId"
+
+# 2. Connect to Graph / Entra ID
+Import-Module Microsoft.Graph.Users # or Microsoft.Graph depending on version
+Connect-MgGraph -Scopes "User.ReadWrite.All"
+
+# 3. Find the cloud user
+$cloudUser = Get-MgUser -Filter "userPrincipalName eq '$userUPN'" -Property OnPremisesImmutableId,Id
+
+if (-not $cloudUser) {
+    Write-Error "Cloud user not found for UPN $userUPN"
+    return
+}
+
+# 4. Compare and update if needed
+if ($cloudUser.OnPremisesImmutableId -ne $immutableId) {
+    Write-Host "Updating cloud user $userUPN with OnPremisesImmutableId = $immutableId"
+    Update-MgUser -UserId $cloudUser.Id -OnPremisesImmutableId $immutableId
+}
+else {
+    Write-Host "Cloud user already has matching OnPremisesImmutableId"
+}
+
+# 5. (Optional) Force a sync of your sync engine (if you’re using e.g. Entra Connect)
+# On your sync server:
+Start-ADSyncSyncCycle -PolicyType Delta
